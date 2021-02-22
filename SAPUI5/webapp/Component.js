81f73da39a4750c3a72098a570591680b8bf093b
mojeda-git <ojeda.cortesm@gmail.com>
2021-02-22 21:10:44 +0000

    test2

--- SAPUI5/webapp/Component.js
+++ SAPUI5/webapp/Component.js
@@ -1,18 +1,20 @@
+// @ts-nocheck
 // @ts-ignore
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "ns/SAPUI5/model/Models",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog"
 ],
     /** 
      * @param {typeof sap.ui.core.UIComponent} UIComponent
     */
-    function (UIComponent, Models, ResourceModel) {
+    function (UIComponent, Models, ResourceModel, HelloDialog) {
 
         return UIComponent.extend("ns.SAPUI5.Component", {
 
             metadata: {
-                manifest : "json"
+                manifest: "json"
                 // "rootView": {
                 //     "viewName": "ns.SAPUI5.view.App",
                 //     "type": "XML",
@@ -34,8 +36,15 @@ sap.ui.define([
                 var i18nModel = new ResourceModel({ bundleName: "ns.SAPUI5.i18n.i18n" });
                 this.setModel(i18nModel, "i18n");
 
+                this._helloDialog = new HelloDialog( this.getRootControl());
 
-
+            },
+            exit: function(){
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+            openHelloDialog: function(){
+                this._helloDialog.open();
             }
         });
 
